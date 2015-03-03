fb11824672a1ce454cf368d36316ab96087c7a72
赵淑楷 <zhaoshukai@xdf.cn>
2015-03-03 15:34:29 +0800

    使用act作为new之外的操作处理入口

--- Controller/DeploymentsController.php
+++ Controller/DeploymentsController.php
@@ -12,6 +12,16 @@ class DeploymentsController extends AppController {
         $this->_needProjectMenuBar = true;
         $this->set('needProjectMenuBar', $this->_needProjectMenuBar);
     }
+
+    public function act($project_id_str) {
+        // 传入值可能以“.js”结尾
+        $project_id_arr = explode('.', $project_id_str);
+        $project_id = $project_id_arr[0];
+        if(isset($project_id_arr[1])) { // 进行预览计算
+            return $this->_estimate($project_id);
+        }
+    }
+
     public function add($project_id) {
 
         if($this->request->is('post')) {
@@ -27,34 +37,36 @@ class DeploymentsController extends AppController {
                     deployment[copy_config_files]:1
                     deployment[email_notify]:0
                     deployment[branch]:master
-                    preview:
+                    preview: // 预览，编辑时的键为edit
                 */
                 $this->view = 'create';
                 $viewBodyJsAction = 'create'; // 特殊的action参数，用于设定页面的body class，详见layouts/default.ctp
 
                 $this->set('BodyJsAction', $viewBodyJsAction);
             }
-        } else {
-            // 独立服务器
-            $this->loadModel('Server');
-            $serverList = $this->Server->find('all', array('conditions' => array('project_id' => $project_id, 'server_group_identifier' => 0)));
-            // 服务器组
-            $this->loadModel('ServerGroup');
-            $this->ServerGroup->bindModel(
-                array('hasMany' => array('Server' => array('className' => 'Server', 'foreignKey' => 'server_group_identifier')))
-            );
-            $groupList = $this->ServerGroup->find('all', array('conditions' => array('ServerGroup.project_id' => $project_id)));
 
-            $this->set('ServerList', $serverList);
-            $this->set('GroupList', $groupList);
+            return;
         }
+
+        // 独立服务器
+        $this->loadModel('Server');
+        $serverList = $this->Server->find('all', array('conditions' => array('project_id' => $project_id, 'server_group_identifier' => 0)));
+        // 服务器组
+        $this->loadModel('ServerGroup');
+        $this->ServerGroup->bindModel(
+            array('hasMany' => array('Server' => array('className' => 'Server', 'foreignKey' => 'server_group_identifier')))
+        );
+        $groupList = $this->ServerGroup->find('all', array('conditions' => array('ServerGroup.project_id' => $project_id)));
+
+        $this->set('ServerList', $serverList);
+        $this->set('GroupList', $groupList);
     }
 
 	/**
 	 * @param        $project_id
 	 * @param string $js 无实际意义，应对前端的请求机制
 	 */
-    public function estimate($project_id, $js='js.js') {
+    protected function _estimate($project_id) {
         /*
          *  utf8:✓
             authenticity_token:gw5yNsLC2NJwoOZZmYeK41LphlxJJ0jDXFQq8xOVV9o=
@@ -67,6 +79,7 @@ class DeploymentsController extends AppController {
             deployment[branch]:master
          */
         $this->layout = '';
+        $this->view   = 'estimate';
 
         $this->loadModel('Repository');
         if(empty( $this->request->data['deployment']['start_revision'])) {
